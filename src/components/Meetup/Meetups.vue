6d292d74334994f16ec9140ec02f315a9b809c1a
Romain Pythoud <romain@MacBook-Pro-de-Romain.local>
2017-12-06 11:38:27 +0100

    06.12.2017

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
-    <p>
-      The home page
-    </p>
-  </div>
+  <v-container>
+    <v-layer row wrap>
+      <v-flex xs12 sm10 md8 offset-sm1 offset-md2> <!-- pour gérer où est la carte -->
+        <v-card class="info">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs5 sm4 md3>
+                <v-card-media
+                     src="https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg "
+                     height="130px"
+                   ></v-card-media>
+              </v-flex>
+              <v-flex xs7 sm8 md9>
+                <v-card-title primary-title>
+                  <div>
+                    <h5 class="white--text mb-0">Meetup in New York</h5>  <!-- si pas de -text, change background de texte -->
+                    <div>17th july 2052</div>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn round color="white" >
+                    <v-icon left color="primary">arrow_forward</v-icon>  <!-- commande pour la flêche-->
+                    view Meetups
+                  </v-btn>
+                </v-card-actions>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layer>
+  </v-container>
 </template>
